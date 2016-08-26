f441b0808728d21f05c15a8b10e9cae5d28d7597
koorchik <koorchik@gmail.com>
2016-08-26 12:24:06 +0300

    New rules

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # eslint-plugin-webbylab
+
+## Supported rules
+
+* no-then - forces use of async / await intead of then.
+* no-window - prohibits usage of "window" global.
+* force-native-method - forces to use native methods instead of lodash.
+* no-numeric-endings-for-variables - prohibits variables with names like "user1" and "user2".
+* no-duplicated-chains - prohibits duplication of long chains like "this.props.user.name"
+* classbody-starts-with-newline - forces/prohibits new line at the beggining of class body
--- index.js
+++ index.js
@@ -5,18 +5,20 @@ module.exports = {
     rules: {
         'no-then': require('./lib/rules/no-then'),
         'no-window': require('./lib/rules/no-window'),
-        'no-number-variable-endings': require('./lib/rules/no-number-variable-endings'),
-        'force-native-methods': require('./lib/rules/force-native-methods')
-
-
+        'no-numeric-endings-for-variables': require('./lib/rules/no-numeric-endings-for-variables'),
+        'force-native-methods': require('./lib/rules/force-native-methods'),
+        'no-duplicated-chains': require('./lib/rules/no-duplicated-chains'),
+        'classbody-starts-with-newline': require('./lib/rules/classbody-starts-with-newline')
     },
     configs: {
     recommended: {
       rules: {
         'webbylab/no-then': 2,
         'webbylab/no-window': 2,
-        'no-number-variable-endings': 2,
-        'force-native-methods': 2
+        'no-numeric-endings-for-variables': 2,
+        'force-native-methods': 2,
+        'no-duplicated-chains': 2,
+        'classbody-starts-with-newline': [2, 'never']
       }
     }
   }
