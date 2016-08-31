329c10299ae1e82c2c68fcf523f834bcc3e29172
Viktor Turskyi <koorchik@gmail.com>
2016-08-31 18:01:32 +0300

    Fix recommended

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ module.exports = {
     configs: {
     recommended: {
       rules: {
-        'webbylab/no-then': 2,
-        'webbylab/no-window': 2,
-        'no-numeric-endings-for-variables': 2,
-        'force-native-methods': 2,
-        'no-duplicated-chains': 2,
-        'classbody-starts-with-newline': [2, 'never']
+        'more/no-then': 2,
+        'more/no-window': 2,
+        'more/no-numeric-endings-for-variables': 2,
+        'more/force-native-methods': 2,
+        'more/no-duplicated-chains': 2,
+        'more/classbody-starts-with-newline': [2, 'never']
       }
     }
   }
